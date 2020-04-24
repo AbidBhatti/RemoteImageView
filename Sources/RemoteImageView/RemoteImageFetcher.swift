bb523c0e71cfe56321c6e32f5e3dd032f15dcfbb
Abid Bhatti <bhattiabid830@gmail.com>
2020-04-24 18:02:32 +0530

    Change init in RemoteImageFetcher

--- Sources/RemoteImageView/RemoteImageFetcher.swift
+++ Sources/RemoteImageView/RemoteImageFetcher.swift
@@ -34,7 +34,7 @@ public class RemoteImageFetcher : ObservableObject {
     
     let url : URL
     
-    public init(url : URL) {
+    public init(from url : URL) {
         self.url = url
     }
     
